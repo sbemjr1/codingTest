9e29ee1ce4660691046e89716d67aefb4a35570b
sbemjr1 <sbemjr1@naver.com>
2023-09-25 13:34:53 +0900

    0925

---
+++ programmers/level2/n^2배열자르기.js
@@ -0,0 +1,11 @@
+const solution = (n, left, right) => {
+  const answer = [];
+
+  for (let i = left; i <= right; i++) {
+    const share = parseInt(i / n); // 행축(y)
+
+    const reminder = i % n; // 열축(x)
+    answer.push(Math.max(share, reminder) + 1);
+  }
+  return answer;
+};
