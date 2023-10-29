d85ae64b02caa888d4ee298976f95145b464fd60
sbemjr1 <sbemjr1@naver.com>
2023-10-29 20:43:08 +0900

    py1029fix

--- baekjoon/class2/1018 체스판 다시 칠하기.py
+++ baekjoon/class2/1018 체스판 다시 칠하기.py
@@ -1,11 +1,58 @@
-N = int(input())
-L = list(range(1,N+1))
+import sys
+input = sys.stdin.readline
 
+n,m=map(int, input().split())
+d=[]
+a=0
+b=0
+aplus=0
+bplus=0
+comp=[]
+result=0
 
-while (len(L)>1):
-    L.pop(0)
-    temp = L.pop(0)
-    L.append(temp)
+for i in range(n) :
+    d.append([])
+    d[i] = input().strip()
 
-
-print(L.pop(0))
+while b < m-8 :
+    while a < n-8 :
+        if d[a][b] == "W" :
+            for i in range(a,a+8) :
+                for j in range(b,b+8) :
+                    if (i+aplus)%2 == 0 :
+                        if (j+bplus)%2 == 0 and d[i][j] != "W" :
+                            result+=1
+                        elif (j+bplus)%2 == 1 and d[i][j] != "B" :
+                            result+=1
+                    elif (i+aplus)%2 == 1 :
+                        if (j+bplus)%2 == 0 and d[i][j] != "B" :
+                            result+=1
+                        elif (j+bplus)%2 == 1 and d[i][j] != "W" :
+                            result+=1
+            a=aplus
+            aplus+=1
+            comp.append(result)
+            result=0
+        elif d[a][b] == "B" :
+            for i in range(a,a+8) :
+                for j in range(b,b+8) :
+                    if (i+aplus)%2 == 0 :
+                        if (j+bplus)%2 == 0 and d[i][j] != "B" :
+                            result+=1
+                        elif (j+bplus)%2 == 1 and d[i][j] != "W" :
+                            result+=1
+                    elif (i+aplus)%2 == 1 :
+                        if (j+bplus)%2 == 0 and d[i][j] != "W" :
+                            result+=1
+                        elif (j+bplus)%2 == 1 and d[i][j] != "B" :
+                            result+=1
+            a=aplus
+            aplus+=1
+            comp.append(result)
+            result=0
+    a=0
+    aplus=0
+    bplus+=1
+    b=bplus
+    
+print(comp)
