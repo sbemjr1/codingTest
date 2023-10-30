0af98a06f9f05e51b5b4259d6ebfc0b813457a13
sbemjr1 <sbemjr1@naver.com>
2023-10-30 15:06:13 +0900

    py1030

--- baekjoon/class2/1018 체스판 다시 칠하기.py
+++ baekjoon/class2/1018 체스판 다시 칠하기.py
@@ -2,7 +2,9 @@ import sys
 input = sys.stdin.readline
 
 n,m=map(int, input().split())
-d=[]
+arr = []
+firstWhiteArr = []
+firstBlackArr = []
 a=0
 b=0
 aplus=0
@@ -11,48 +13,51 @@ comp=[]
 result=0
 
 for i in range(n) :
-    d.append([])
-    d[i] = input().strip()
+    arr.append([])
+    arr[i] = input().strip()
 
-while b < m-8 :
-    while a < n-8 :
-        if d[a][b] == "W" :
-            for i in range(a,a+8) :
-                for j in range(b,b+8) :
-                    if (i+aplus)%2 == 0 :
-                        if (j+bplus)%2 == 0 and d[i][j] != "W" :
-                            result+=1
-                        elif (j+bplus)%2 == 1 and d[i][j] != "B" :
-                            result+=1
-                    elif (i+aplus)%2 == 1 :
-                        if (j+bplus)%2 == 0 and d[i][j] != "B" :
-                            result+=1
-                        elif (j+bplus)%2 == 1 and d[i][j] != "W" :
-                            result+=1
-            a=aplus
-            aplus+=1
-            comp.append(result)
-            result=0
-        elif d[a][b] == "B" :
-            for i in range(a,a+8) :
-                for j in range(b,b+8) :
-                    if (i+aplus)%2 == 0 :
-                        if (j+bplus)%2 == 0 and d[i][j] != "B" :
-                            result+=1
-                        elif (j+bplus)%2 == 1 and d[i][j] != "W" :
-                            result+=1
-                    elif (i+aplus)%2 == 1 :
-                        if (j+bplus)%2 == 0 and d[i][j] != "W" :
-                            result+=1
-                        elif (j+bplus)%2 == 1 and d[i][j] != "B" :
-                            result+=1
-            a=aplus
-            aplus+=1
-            comp.append(result)
-            result=0
+while b <= m-8 :
+    while a <= n-8 :
+        for i in range(a,a+8) :
+            for j in range(b,b+8) :
+                if (i+aplus)%2 == 0 :
+                    if (j+bplus)%2 == 0 and arr[i][j] != "W" :
+                        result+=1
+                    elif (j+bplus)%2 == 1 and arr[i][j] != "B" :
+                        result+=1
+                elif (i+aplus)%2 == 1 :
+                    if (j+bplus)%2 == 0 and arr[i][j] != "B" :
+                        result+=1
+                    elif (j+bplus)%2 == 1 and arr[i][j] != "W" :
+                        result+=1
+        firstWhiteArr.append(result)
+        result=0
+    
+        for i in range(a,a+8) :
+            for j in range(b,b+8) :
+                if (i+aplus)%2 == 0 :
+                    if (j+bplus)%2 == 0 and arr[i][j] != "B" :
+                        result+=1
+                    elif (j+bplus)%2 == 1 and arr[i][j] != "W" :
+                        result+=1
+                elif (i+aplus)%2 == 1 :
+                    if (j+bplus)%2 == 0 and arr[i][j] != "W" :
+                        result+=1
+                    elif (j+bplus)%2 == 1 and arr[i][j] != "B" :
+                        result+=1
+        aplus+=1
+        a=aplus
+        firstBlackArr.append(result)
+        result=0
     a=0
     aplus=0
     bplus+=1
     b=bplus
-    
-print(comp)
+
+firstBlackArr.sort()
+firstWhiteArr.sort()
+
+if firstWhiteArr[0] < firstBlackArr[0] :
+    print(firstWhiteArr[0])
+else : 
+    print(firstBlackArr[0])
