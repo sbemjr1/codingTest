9e67889451c7c00604839714c563029ea18e2d54
sbemjr1 <sbemjr1@naver.com>
2023-11-04 12:49:03 +0900

    py1104

--- baekjoon/class2/10816 숫자 카드2.py
+++ baekjoon/class2/10816 숫자 카드2.py
@@ -2,31 +2,26 @@ import sys
 input = sys.stdin.readline
 
 N=int(input())
-NArray = list(map(int,input().split()))
-Ncopy = NArray[:]
-
+NArray = sorted(list(map(int,input().split())))
 M=int(input())
 MArray = list(map(int,input().split()))
 
-Ncopy = list(set(Ncopy))
+def binary(n, NArray, left, right) :
+    if left > right :
+        return 0
+    mid = (left+right)//2
+    if n == NArray[mid] :
+        return NArray[left:right+1].count(n)
+    elif n > NArray[mid] :
+        return binary(n, NArray, mid+1, right)
+    else :
+        return binary(n, NArray, left, mid-1)
 
-Ncopy=Ncopy.sort()
+n_dic= {}
+for n in NArray :
+    left = 0
+    right = len(NArray) - 1
+    if n not in n_dic :
+        n_dic[n] = binary(n,NArray,left,right)
 
-print(Ncopy)
-# for num in MArray :
-#     left,right = 0, len(Ncopy)
-#     isExist = False
-
-#     while left <= right :
-#         mid = (left+right)//2
-#         if num == Ncopy[mid] :
-#             print(NArray.count(num))
-#             isExist=True
-#             break
-#         elif num > NArray[mid] :
-#             left=mid+1
-#         elif num < NArray[mid] :
-#             right=mid-1
-#     if not isExist :
-#         print(0)
-    
+print(' '.join(str(n_dic[x]) if x in n_dic else '0' for x in MArray))
