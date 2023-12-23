a028bf635095e9c79212b267157e283f94351b84
sbemjr1 <sbemjr1@naver.com>
2023-12-23 18:45:13 +0900

    py:12-23 그리디 알고리즘 복습

---
+++ Algorithm/greedy/모험가 길드 복습.py
@@ -0,0 +1,14 @@
+n = int(input())
+data = list(map(int, input().split()))
+data.sort()
+
+count = 0
+result = 0
+
+for i in data:
+    count += 1
+    if count >= i:
+        result += 1
+        count = 0
+
+print(result)
