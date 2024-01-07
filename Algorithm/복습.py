2d5f682619f35ca80d9cf6cdb8ec706fc7be4966
sbemjr1 <sbemjr1@naver.com>
2024-01-07 21:54:11 +0900

    feat:재귀 복습 중

--- Algorithm/복습.py
+++ Algorithm/복습.py
@@ -1,27 +1,20 @@
-from collections import deque
+n = int(input())
+c = 0
 
-n, m = map(int, input().split())
+def recursive_q(n,c):
+    print('____'*(c) + '\"재귀함수가 뭔가요?\"')
+    print('____'*(c) + '\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.')
+    print('____'*(c) + '마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.')
+    print('____'*(c) + '그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"')
+    n -= 1
+    c += 1
+    if n < 1:
+        print('____'*(c) + '\"재귀함수가 뭔가요?\"')
+        print('____'*(c) + '\"재귀함수는 자기 자신을 호출하는 함수라네\"')
+        print('____'*(c) + '라고 답변하였지.')
+    else:
+        recursive_q(n,c)
+    print('____'*(c - 1) + '라고 답변하였지.')
 
-graph = []
-for _ in range(n) :
-    graph.append(list(map(int, input())))
-
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-
-def bfs(x, y) :
-    queue = deque()
-    queue.append((x, y))
-
-    while queue :
-        x, y = queue.popleft()
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if nx < 0 or nx >= n or ny < 0 or ny >= m:
-                continue
-            if graph[nx][ny] == 0:
-                continue
-            if graph[nx][ny]:
-                graph[nx][ny] = graph[x][y] + 1
-                queue.append((nx, ny))
+print('어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.')
+recursive_q(n,c)
